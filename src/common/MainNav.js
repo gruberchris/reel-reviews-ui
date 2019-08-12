653d684f08a6151cf60330cfa9f0945e3a87cfc9
Chris Gruber <chris@chrisgruber.com>
2019-08-12 14:06:43 -0500

    enhance navbar appearance a little

--- src/common/MainNav.js
+++ src/common/MainNav.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const MainNav = () => {
   return (
     <div>
       <Navbar color="light" light>
         <NavbarBrand to="/" tag={Link}>
-          Find Movies
+          Reel Reviews
         </NavbarBrand>
-        <Nav navbar>
+        <Nav>
+          <NavItem>
+            <NavLink to="/" tag={Link}>
+              <FontAwesomeIcon icon={faSearch} /> Find Movies
+            </NavLink>
+          </NavItem>
           <NavItem>
             <NavLink to="/Favs" tag={Link}>
-              Favorites
+              <FontAwesomeIcon icon={faStar} /> Favorites
             </NavLink>
           </NavItem>
         </Nav>
