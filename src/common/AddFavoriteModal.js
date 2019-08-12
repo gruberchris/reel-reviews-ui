3330a828a4f9365df7711a10791916894389d8db
Chris Gruber <chris@chrisgruber.com>
2019-08-12 13:24:17 -0500

    clean up the modals appearance

--- src/common/AddFavoriteModal.js
+++ src/common/AddFavoriteModal.js
@@ -19,45 +19,59 @@ const AddFavoriteModal = ({ show, movie, onClose, onSubmit }) => {
 
   return (
     <Modal isOpen={show} toggle={onClose}>
-      <ModalHeader toggle={onClose}>Add Favorite</ModalHeader>
+      <ModalHeader toggle={onClose}>
+        Rate, Review & Create A Favorite
+      </ModalHeader>
       <ModalBody>
         <div>
           <h4>{`${movie.Title} (${movie.Year})`}</h4>
-          <p>{movie.shortPlot}</p>
         </div>
         <br />
         <Form onSubmit={handleSubmit}>
           <FormGroup row>
-            <Label sm={2}>Your Movie Rating</Label>
-            <Col sm={10}>
+            <Label sm={{ size: 10, offset: 1 }} for="ratingInput">
+              Your Movie Rating
+            </Label>
+            <Col sm={{ size: 10, offset: 1 }}>
               <Input
+                id="ratingInput"
                 name="rating"
                 type="number"
                 placeholder="How many stars would you rate this movie?"
                 onChange={handleInputChange}
                 value={inputs.rating || ''}
-                required
               />
             </Col>
           </FormGroup>
 
           <FormGroup row>
-            <Label sm={2}>Your Movie Review</Label>
-            <Col sm={10}>
+            <Label sm={{ size: 10, offset: 1 }} for="reviewInput">
+              Your Movie Review
+            </Label>
+            <Col sm={{ size: 10, offset: 1 }}>
               <Input
+                id="reviewInput"
                 name="review"
-                type="text"
+                type="textarea"
+                rows="5"
                 placeholder="What do you have to say about this movie?"
                 onChange={handleInputChange}
                 value={inputs.review || ''}
-                required
               />
             </Col>
           </FormGroup>
-          <Button onClick={onClose}>Cancel</Button>
-          <Button color="success" type="submit">
-            Save
-          </Button>
+
+          <FormGroup>
+            <Col sm={{ size: 'auto', offset: 7 }}>
+              <Button color="danger" onClick={onClose}>
+                Cancel
+              </Button>
+              {'   '}
+              <Button color="primary" type="submit">
+                Save
+              </Button>
+            </Col>
+          </FormGroup>
         </Form>
       </ModalBody>
     </Modal>
